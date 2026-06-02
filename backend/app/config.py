a5c2b07d9ffa9051d35db61d77d78a7f12e82d71
apratxxm <apratxmwork@gmail.com>
2026-06-02 21:20:04 +0530

    app\config

---
+++ backend/app/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    GROQ_API_KEY: str
+    
+    SMTP_SERVER: str = "smtp.gmail.com"
+    SMTP_PORT: int = 587
+    SENDER_EMAIL: str
+    SENDER_PASSWORD: str
+    
+    DATABASE_URL: str
+    
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+settings = Settings()
