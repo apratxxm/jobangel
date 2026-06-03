c963f981391094e212414452c973253242cd8c82
apratxxm <apratxmwork@gmail.com>
2026-06-04 01:55:19 +0530

    scrape route

---
+++ backend/app/routers/scrape.py
@@ -0,0 +1,17 @@
+
+from fastapi import BackgroundTasks, Depends, APIRouter
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.scrapers.apify_scraper import run_apify_scraper
+
+router = APIRouter()
+
+@router.post("/run")
+def trigger_scraper(background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    """
+    Triggers the Apify scraper as a background task
+    """
+    #We pass it to background_tasks si the API doesn't freeze while waiting for Apify
+    background_tasks.add_task(run_apify_scraper,db)
+
+    return{"status": "ok", "message": "Scraper running in background, Check terminal for logs."}
